ffb15b8bf5a5955011d55815c82b507013efce94
alex <alexbarbu240@gmail.com>
2022-12-05 12:34:46 +0200

    testing database connection done

---
+++ src/Codecool.CodecoolShop/Controllers/TestController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Codecool.CodecoolShop.Controllers
+{
+    public class TestController : Controller
+    {
+
+        private IConfiguration Configuration;
+
+        public TestController(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+
+        public string ConnectionString => Configuration.GetConnectionString("DefaultConnection");
+
+        public bool TestConnection()
+        {
+            using var connection = new SqlConnection(ConnectionString);
+            try
+            {
+                connection.Open();
+                connection.Close();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        private void EnsureConnectionSuccessful()
+        {
+            if (!TestConnection())
+            {
+                Environment.Exit(1); // if we cannot connect to DB we terminate the app
+            }
+
+        }
+        public IActionResult Index()
+        {
+            EnsureConnectionSuccessful();
+            return View();
+        }
+    }
+
+
+}
---
+++ src/Codecool.CodecoolShop/Services/DbManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Codecool.CodecoolShop.Services
+{
+    public class DbManager
+    {
+        //public string ConnectionString => ConfigurationManager.AppSettings["connectionString"];
+
+        //public bool TestConnection()
+        //{
+        //    using var connection = new SqlConnection(ConnectionString);
+        //    try
+        //    {
+        //        connection.Open();
+        //        connection.Close();
+        //        return true;
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine(e.Message);
+        //        return false;
+        //    }
+        //}
+
+        
+    }
+}
